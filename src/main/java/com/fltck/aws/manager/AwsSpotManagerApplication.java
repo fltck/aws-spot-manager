48054290152918b0390db1b0d051145ba9dea63e
Brent Lemons <brent_lemons@yahoo.com>
2017-12-09 14:55:54 -0600

    start the spot manager

---
+++ src/main/java/com/fltck/aws/manager/AwsSpotManagerApplication.java
@@ -0,0 +1,12 @@
+package com.fltck.aws.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSpotManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsSpotManagerApplication.class, args);
+	}
+}
